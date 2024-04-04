db329f968dcd278a4138cff6bf1309446abd01a6
Ivan Kubyshev <ikubyshev@dc2b.ru>
2024-04-04 17:18:00 +0300

    Co-authored-by: Dark <Prionlante@users.noreply.github.com> Co-authored-by: Alexey Ivashkin <RomDmitriy@users.noreply.github.com>

--- uilex.py
+++ uilex.py
@@ -1,7 +1,6 @@
-import time
 from NumberRecognition import getFuckingNuumber
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton, QLineEdit
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import QRect
 import sys
@@ -11,23 +10,14 @@ from PyQt5 import QtCore
 import numpy as np
 from yolo import yolo_car_checker
 
-# class VideoThread(QThread):
-#     change_pixmap_signal = pyqtSignal(np.ndarray)
-
-#     def run(self):
-#         # capture from web cam
-#         cap = cv2.VideoCapture('./Object_Detection/test/video.mp4')
-#         while True:
-#             ret, cv_img = cap.read()
-#             if ret:
-#                 self.change_pixmap_signal.emit(cv_img)
-
 class App(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.counter = 0
         self.setWindowTitle("Qt live label demo")
         self.setFixedWidth(800)
-        self.setFixedHeight(600)
+        self.setFixedHeight(460)
         self.display_width = 370
         self.display_height = 210
 
@@ -39,27 +29,24 @@ class App(QWidget):
         # создаем второе видео (верхнее правое)
         self.recognition_layout = QLabel(self)
         self.recognition_layout.resize(self.display_width, self.display_height)
-        self.recognition_layout.setGeometry(QRect(410, 10, 790, 220))
+        self.recognition_layout.setGeometry(QRect(410, 10, 390, 220))
 
         # создаем третье видео (нижнее левое)
-        self.changeme_layout = QLabel(self)
-        self.changeme_layout.resize(self.display_width, self.display_height)
-        self.changeme_layout.setGeometry(QRect(20, 230, 790, 220))
-
-        # создаем блок информации
-        self.information_label = QLabel(self)
-        self.information_label.setGeometry(410, 160, 790, 160)
-        self.information_label.setText('Информация')
-
-        self.plate_number_layout = QLabel(self)
-        self.plate_number_layout.setGeometry(410, 175, 790, 175)
-        self.plate_number_layout.setText('тута ваня')
+        self.recognition_with_filters_layout = QLabel(self)
+        self.recognition_with_filters_layout.resize(self.display_width, self.display_height)
+        self.recognition_with_filters_layout.setGeometry(QRect(20, 230, 390, 220))
 
         # Создаем кнопку для начала детекта
-        self.start_button = QPushButton(self)
-        self.start_button.setGeometry(QtCore.QRect(410, 500, 100, 20))
-        self.start_button.setObjectName("start_button")
-        self.start_button.clicked.connect(self.run)
+        self.next_frame = QPushButton(self)
+        self.next_frame.setText('След')
+        self.next_frame.setGeometry(QtCore.QRect(680, 230, 100, 25))
+        self.next_frame.setObjectName("start_button")
+        self.next_frame.clicked.connect(self.run)
+        self.previous_frame = QPushButton(self)
+        self.previous_frame.setText('Пред')
+        self.previous_frame.setGeometry(QtCore.QRect(570, 230, 100, 25))
+        self.previous_frame.setObjectName("previous_frame")
+        self.previous_frame.clicked.connect(self.go_back)
 
         # # create the video capture thread
         # self.thread = VideoThread()
@@ -68,16 +55,26 @@ class App(QWidget):
         # # start the thread
         # self.thread.run()
 
+        # Фильтры
+        self.filters = []
+        self.filter_label = QLabel(f'Кадр {self.counter}', self)
+        self.filter_label.setGeometry(410, 230, 100, 25)  # Позиция и размеры фильтр-лейбла
+        self.filter_input = QLineEdit(self)
+        self.filter_input.setGeometry(410, 260, 370, 25)  # Позиция и размеры поля для ввода номерного знака
+        self.filter_input.textChanged.connect(self.on_plate_filter_changed)
+
+    # Фильтр
+    def on_plate_filter_changed(self, plate_filter):
+        self.filters = plate_filter.split()
+        print("Новый фильтр:", self.filters)
+
     @pyqtSlot(np.ndarray)
     def process_image(self, cv_img):
         """Обрабатывает и выводит в приложение все изображения"""
-        print(1111)
-        self.original_video_label.setPixmap(self.convert_cv_qt(cv_img))
-        return
 
         # Обработка через нейронку
         # Получение обрезанных автомобилей
-        cars, yolo_mat_img, sqr_arr = yolo_car_checker(cv_img)
+        cars, yolo_mat_img, sqr_arr = yolo_car_checker(cv_img.copy())
         #sqr_arr.append(tuple((xmin, ymin), (xmax, ymax)))
 
         # Защита если Серега вернул хуйню
@@ -91,33 +88,33 @@ class App(QWidget):
             # Добавление в массив номера автомобиля
             cars[2].append(getFuckingNuumber(cars[1][i]))
 
-        # Наложение фильтров @todo
-        carsFiltered = [[], list(), []]
-        hardcode_filter = 'car'
+        # Наложение фильтров
+        cars_filtered = [[], list(), []]
         car_detected_cv = cars[1]
-        buf = []
         filtered_img = cv_img.copy()
         for i in range(len(car_detected_cv)):
-            if (cars[0][i] == hardcode_filter):
-                carsFiltered[0].append(cars[0][i])
-                carsFiltered[1].append(cars[1][i])
-                carsFiltered[2].append(cars[2][i])
-                # buf.append(cv2.resize(i, (64,64)))
+            if cars[0][i] in self.filters or cars[2][i] in self.filters:
+                cars_filtered[0].append(cars[0][i])
+                cars_filtered[1].append(cars[1][i])
+                cars_filtered[2].append(cars[2][i])
                 cv2.rectangle(filtered_img, sqr_arr[i][0], sqr_arr[i][1], (255, 0, 255), 3)  # box
+        pink_car = filtered_img.copy()
 
         # Вывод для дебага
-        print('===Без фильтров===')
+        print('==========Без фильтров========')
         print('Типы автомобилей:', cars[0])
         print('Номерной знак автомобилей:', cars[2])
-        print('===С фильтрами====')
-        print('Типы автомобилей:', carsFiltered[0])
-        print('Номерной знак автомобилей:', carsFiltered[2])
+        print('==========С фильтрами=========')
+        print('Типы автомобилей:', cars_filtered[0])
+        print('Номерной знак автомобилей:', cars_filtered[2])
+
+        # Вывод в таблицу @todo
+
 
         # Вывод всех изображений
-        #self.original_video_label.setPixmap(self.convert_cv_qt(cv_img))        # Ориг кадр
-        self.recognition_layout.setPixmap(self.convert_cv_qt(yolo_mat_img))     # Кадр с квадратиками авто
-        self.changeme_layout.setPixmap(self.convert_cv_qt(filtered_img))        # Кадр с квадратиками авто с фильтрами
-        print('function ended')
+        self.original_video_label.setPixmap(self.convert_cv_qt(cv_img))             # Ориг кадр
+        self.recognition_layout.setPixmap(self.convert_cv_qt(yolo_mat_img))         # Кадр с квадратиками авто
+        self.recognition_with_filters_layout.setPixmap(self.convert_cv_qt(pink_car))# Кадр с квадратиками авто с фильтрами
 
     def convert_cv_qt(self, cv_img):
         """Convert from an opencv image to QPixmap"""
@@ -128,23 +125,40 @@ class App(QWidget):
         p = convert_to_Qt_format.scaled(self.display_width, self.display_height, Qt.KeepAspectRatio)
         return QPixmap.fromImage(p)
     
+    def go_back(self):
+        # @todo out of index guard
+        self.counter -= 2
+        self.run()
+
     # Это наш мейн
     def run(self):
+        self.filter_label.setText(f'В процессе..')
         # capture from video
-        cap = cv2.VideoCapture('./Object_Detection/test/vehicle-counting.mp4')
-        while True:
-            ret, cv_img = cap.read()
-            if ret:
-                print(123)
-                self.process_image(cv_img)
-            else:
-                break
+        cap = cv2.VideoCapture('./Object_Detection/test/Untitled.mp4')
+        local_counter = 0
+        good = False
+        cv_img = 0
+
+        while local_counter <= self.counter:
+            # @todo out of index guard
+            local_counter += 1
+            good, cv_img = cap.read()
+        
+        print(cv_img)
+        
+        # иф аут оф индекс
+        if good:
+            self.process_image(cv_img)
+            self.counter += 1
+        else:
+            print('Не хорошо(((')
+        
+        self.filter_label.setText(f'Кадр {self.counter}')
+
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     a = App()
     a.show()
-    # t = QtCore.QTimer()
-    # t.singleShot(0,a.run)
     sys.exit(app.exec_())
